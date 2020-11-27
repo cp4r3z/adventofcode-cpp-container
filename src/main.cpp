21ffc26d80bae8d358b7665b9a48f76b5cad965d
cp4r3z <cp4r3z@gmail.com>
2020-11-27 03:45:20 +0000

    Commenting code

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,34 @@
 #include <string>
 #include <vector>
 #include <iostream> // cout
-#include <fstream> // ifstream
-#include <numeric> // accumulate
+#include <fstream>  // ifstream
+#include <numeric>  // accumulate
 
 #include "solution01.h"
 
 using namespace std;
 
+string INPUT_PATH = "src/solution01_input.txt";
+
 int main()
 {
+    // Read the input file into a vector
     vector<int> masses;
     string line;
 
-    ifstream inputFile("src/solution01_input.txt");
+    ifstream inputFile(INPUT_PATH);
     while (getline(inputFile, line))
     {
         masses.push_back(stoi(line));
     }
     inputFile.close();
 
-    auto ConvertToFuel = [](int a, int b) {
-        return move(a) + GetFuelRequirement(b);
+    // Operation to sum the fuel requirements for each mass
+    auto ConvertToFuel = [](int acc, int mass) {
+        return move(acc) + GetFuelRequirement(mass);
     };
 
+    // Accumulate all fuel requirements
     int totalFuelRequirement = accumulate(masses.begin(), masses.end(), 0, ConvertToFuel);
 
     cout << "Day 1 Solution Part 1: Fuel Requirement: " << totalFuelRequirement << "\n\n";
